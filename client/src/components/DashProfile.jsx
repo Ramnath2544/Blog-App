a7d575bcab86b645dfa0fc393e0925007c81662c
Ramnath <ramnath2544@gmail.com>
2026-03-21 09:56:29 +0530

    Check Demo file console

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,40 +1,70 @@
-import { useSelector } from 'react-redux';
-import { Button, TextInput } from 'flowbite-react';
+import { useSelector } from "react-redux";
+import { Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 
 export default function DashProfile() {
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+  useEffect(() => {
+    if (imageFile) {
+   uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    console.log("Uploading image...");
+  };
 
   return (
-    <div className='max-w-lg mx-auto p-3 w-full'>
-      <h1 className='text-center font-semibold text-3xl my-7'>Profile</h1>
-      <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer overflow-hidden rounded-full shadow-md'>
+    <div className="max-w-lg mx-auto p-3 w-full">
+      <h1 className="text-center font-semibold text-3xl my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="file"
+          accept="image/*"
+          onChange={handleImageChange}
+          ref={filePickerRef}
+          hidden
+        />
+        <div
+          className="w-32 h-32 self-center cursor-pointer overflow-hidden rounded-full shadow-md"
+          onClick={() => filePickerRef.current.click()}
+        >
           <img
-            src={currentUser.profilePicture}
-            alt='user'
-            className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'
+            src={imageFileUrl || currentUser.profilePicture}
+            alt="user"
+            className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
           />
         </div>
         <TextInput
-          type='text'
-          id='username'
-          placeholder='username'
+          type="text"
+          id="username"
+          placeholder="username"
           defaultValue={currentUser.username}
         />
         <TextInput
-          type='email'
-          id='email'
-          placeholder='email'
+          type="email"
+          id="email"
+          placeholder="email"
           defaultValue={currentUser.email}
         />
-        <TextInput type='password' id='password' placeholder='password' />
-        <Button type='submit' gradientduotone='purpleToBlue' outline>
+        <TextInput type="password" id="password" placeholder="password" />
+        <Button type="submit" gradientduotone="purpleToBlue" outline>
           Update Profile
         </Button>
       </form>
-      <div className='text-red-500 flex justify-between mt-5'>
-        <span className='cursor-pointer'>Delete Account</span>
-        <span className='cursor-pointer'>Sign Out</span>
+      <div className="text-red-500 flex justify-between mt-5">
+        <span className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
       </div>
     </div>
   );
