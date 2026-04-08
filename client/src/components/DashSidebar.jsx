86d86f9a8c982d37cf083dfd05c207f030e2f041
Ramnath <ramnath2544@gmail.com>
2026-04-08 18:11:40 +0530

    Add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -7,6 +7,7 @@ import {
 import {
   HiAnnotation,
   HiArrowSmRight,
+  HiChartPie,
   HiDocumentText,
   HiOutlineUserGroup,
   HiUser,
@@ -51,6 +52,16 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
       <SidebarItems>
         <SidebarItemGroup className='flex flex-col gap-1 '>
+          {currentUser && currentUser.isAdmin && (
+            <SidebarItem
+              as={Link}
+              to='/dashboard?tab=dash'
+              active={tab === 'dash' || !tab}
+              icon={HiChartPie}
+            >
+              Dashboard
+            </SidebarItem>
+          )}
           <SidebarItem
             as={Link}
             to='/dashboard?tab=profile'
